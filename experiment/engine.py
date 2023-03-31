830e810b047f20932225a71be02a0ad7f6ba087b
Jasper van den Bosch <japsai@gmail.com>
2023-03-31 16:19:34 +0100

    separate first flip

--- experiment/engine.py
+++ experiment/engine.py
@@ -3,17 +3,19 @@ https://psychopy.org/coder/codeStimuli.html
 https://psychopy.org/general/timing/detectingFrameDrops.html#warn-me-if-i-drop-a-frame
 """
 from __future__ import annotations
-from typing import List
-from typing import Tuple, Dict
+from typing import TYPE_CHECKING, Tuple, Dict, List, Union
 from psychopy.monitors import Monitor
 from psychopy.event import waitKeys
 from psychopy.core import wait
 from psychopy.visual import Window, TextStim
+from psychopy.visual.ratingscale import RatingScale
 from psychopy.visual.line import Line
 from psychopy.visual.rect import Rect
 from psychopy.visual.shape import ShapeStim
+from experiment.dummy import DummyStim
 from experiment.ports import TriggerInterface, FakeTriggerPort, createTriggerPort
-
+if TYPE_CHECKING:
+    Stimulus = Union[TextStim, DummyStim, ShapeStim, Rect]
 
 class PsychopyEngine(object):
 
@@ -23,7 +25,8 @@ class PsychopyEngine(object):
     ## stimuli
     target1: TextStim
     target2: TextStim
-    target2_squares: List[Rect]
+    squares: List[Rect]
+    target2_dummy: DummyStim
     mask: TextStim
     fixCross: ShapeStim
 
@@ -56,6 +59,7 @@ class PsychopyEngine(object):
             units='deg'
         )
         scaling = settings['mon_resolution'][0] / self.win.size[0]
+        print(scaling)
         if scaling == 0.5: 
             print('Looks like a retina display')
         elif scaling != 0.0:
@@ -64,13 +68,14 @@ class PsychopyEngine(object):
     def loadStimuli(self, squareSize: float, squareOffset: int, fixSize: float):
         self.target1 = TextStim(self.win, height=1, units='deg')
         self.target2 = TextStim(self.win, height=1, units='deg')
+        self.target2_dummy = DummyStim()
         self.mask = TextStim(self.win, height=1, units='deg')
         square_size = (squareSize, squareSize)
         o = squareOffset
         positions = [(-o, -o), (o, -o), (-o, o), (o, o)]
-        self.target2_squares = []
+        self.squares = []
         for pos in positions:
-            self.target2_squares.append(Rect(
+            self.squares.append(Rect(
                 self.win,
                 size=(square_size, square_size),
                 units='deg',
@@ -122,6 +127,16 @@ class PsychopyEngine(object):
             viewPixBulbSize=7.0
         )
 
+    def drawFlipAndTrigger(self, stims: List[Stimulus], duration: int, triggerNr: int):
+        for stim in stims:
+            stim.draw()
+        self.port.trigger(triggerNr)
+        self.win.flip()
+        for _ in range(duration-1):
+            for stim in stims:
+                stim.draw()
+            self.win.flip()
+
     def displayEmptyScreen(self, duration: int) -> None:
         for _ in range(duration):
             self.win.flip()
@@ -131,10 +146,7 @@ class PsychopyEngine(object):
         Displays the first target (T1) consisting of either the string 'OXXO' or 'XOOX'
         '''
         self.target1.text = val
-        self.target1.draw()
-        self.port.trigger(triggerNr)
-        for _ in range(duration):
-            self.win.flip()
+        self.drawFlipAndTrigger([self.target1], duration, triggerNr)
 
     def displayT2(self, val: str, triggerNr: int, duration: int) -> None:
         '''
@@ -143,18 +155,12 @@ class PsychopyEngine(object):
         Parameters:
             T2_present bool: True for present or False for absent
         '''
-        for square in self.target2_squares:
-            square.draw()
-
         if len(val):
-            self.target2.text = val #random.choice(constants.target2_strings)
-            self.target2.draw()
+            target2 = self.target2
         else:
-            self.target2.text = ''
-
-        self.port.trigger(triggerNr)
-        for _ in range(duration):
-            self.win.flip()
+            target2 = self.target2_dummy
+        target2.text = val
+        self.drawFlipAndTrigger([target2]+self.squares, duration, triggerNr)
 
     def displayMask(self, val: str, duration: int) -> None:
         '''
@@ -162,56 +168,68 @@ class PsychopyEngine(object):
         The selection and order of consonants is ramdomly chosen at every execution.
         '''
         self.mask.text = val
-        self.mask.draw()
         for _ in range(duration):
+            self.mask.draw()
             self.win.flip()
 
     def displayFixCross(self, duration: int):
-        self.fixCross.draw()
         for _ in range(duration):
+            self.fixCross.draw()
             self.win.flip()
 
     def promptIdentity(self, prompt: str, options: Tuple[str, str], trigger: int) -> Tuple[int, int]:
         while True:
             ## This loop is a trick to force a choice; if the dummy middle choice is chosen,
             ## we simply create a new RatingScale
-            rating_scaleT1 = RatingScale(SCREEN, noMouse=True,
-                choices=['OO', '', 'XX'], markerStart=1, #labels=['OO', '', 'XX'],
-                scale=task1_text, acceptKeys='space', lineColor='DarkGrey',
+            rating_scaleT1 = RatingScale(
+                self.win, noMouse=True,
+                choices=['OO', '', 'XX'], markerStart=1,
+                scale=prompt, acceptKeys='space', lineColor='DarkGrey',
                 markerColor='DarkGrey', pos=(0.0, 0.0), showAccept=False)
             rating_scaleT1.draw()
-            SCREEN.flip()
+            self.win.flip()
             while rating_scaleT1.noResponse:
                 rating_scaleT1.draw()
-                SCREEN.flip()
+                self.win.flip()
             if rating_scaleT1.getRating() != '':
                 ## valid choice; continue
                 break
 
+        print(rating_scaleT1.getRating())
+        print(rating_scaleT1.getRT())
+
         # get and return the rating int, int
-        return [rating_scaleT1.getRating(), rating_scaleT1.getRT()]
+        return rating_scaleT1.getRating(), rating_scaleT1.getRT()
     
-    def promptVisibility(self, prompt: str, labels: Tuple[str, str], scale_length: int, init: int, trigger: int) -> Tuple[int, int]:
-    # the rating scale has to be re-initialized in every function call, because
-    # the marker start can't be randomized and updated when using the same rating
-    # scale object again and again.
-    # The marker start needs to be defined randomly beforehand
-        scale_length = 21 # the maximum visibiliy rating
-        task2_text = 'Please indicate the visibility of the number word \n by choosing a rating on the scale below.\n' \
-                    'Press \'space\' to confirm.\n\n'
-        random_init = random.choice(range(scale_length))
-        rating_scaleT2 = RatingScale(SCREEN, low=0, high=scale_length-1, labels=['nothing', 'maximal visibility'],
-                                            acceptKeys='space', scale=task2_text, noMouse=True, lineColor='DarkGrey',
-                                            markerColor='LightGrey', pos=(0.0, 0.0), showAccept=False, markerStart=random_init)
+    def promptVisibility(self, prompt: str, labels: Tuple[str, str], scale_length: int, init: int, triggerNr: int) -> Tuple[int, int]:
+        # the rating scale has to be re-initialized in every function call, because
+        # the marker start can't be randomized and updated when using the same rating
+        # scale object again and again.
+        # The marker start needs to be defined randomly beforehand
+        rating_scaleT2 = RatingScale(
+            self.win, low=0,
+            high=scale_length-1,
+            labels=['nothing', 'maximal visibility'],
+            acceptKeys='space',
+            scale=prompt,
+            noMouse=True,
+            lineColor='DarkGrey',
+            markerColor='LightGrey',
+            pos=(0.0, 0.0),
+            showAccept=False,
+            markerStart=init
+        )
 
         rating_scaleT2.draw()
-        p.trigger(Triggers.taskT2visibility)
-        SCREEN.flip()
+        self.port.trigger(triggerNr)
+        self.win.flip()
 
         # Show scale and instruction und confirmation of rating is done
         while rating_scaleT2.noResponse:
             rating_scaleT2.draw()
-            SCREEN.flip()
+            self.win.flip()
+        print(rating_scaleT2.getRating())
+        print(rating_scaleT2.getRT())
 
         # get and return the rating
-        return [rating_scaleT2.getRating(), rating_scaleT2.getRT()]
+        return rating_scaleT2.getRating(), rating_scaleT2.getRT()
