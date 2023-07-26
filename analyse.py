c6d03eb9bc21c6feea8959838ac8481d55e39f30
Jasper van den Bosch <japsai@gmail.com>
2023-07-26 13:03:34 +0100

    improved importing of events

--- analyse.py
+++ analyse.py
@@ -1,14 +1,23 @@
 """Analysis of pilot data
 
+## Pilot idiosyncrasies
+- [('train', 'single'), ('test', 'dual')] 
+- no bad channels
+
+## TODO
+
+- 
+
+
+trial numbers spreadsheet: https://docs.google.com/spreadsheets/d/14jrOEcPnLSVjfQn3yfuNDqvA0M7qz-dA0L19HOADiRs/edit#gid=0
+
 EX1: left mastoid
 EX2: right mastoid
-EX3: bottom VEOG
-EX4: top VEOG
-EX5: left HEOG
-EX6: right HEOG
+EX3: bottom HEOG
+EX4: top HEOG
+EX5: left VEOG
+EX6: right VEOG
 
-trigger offset:
-Decimal: 65280 Binary: 1111111100000000
 
 In order to analyze the brain events underlying this bimodal distribu- tion, 
 we compared the ERPs evoked by T2 during the attentional blink (short SOA, dual task) 
@@ -20,37 +29,38 @@ evoked by T2 by subtracting the ERPs evoked when T2 was absent and replaced by a
  The remaining trials were averaged in synchrony with T2 onset (or T1 onset for T1-evoked ERPs),
    digitally transformed to an average reference, band-pass filtered (0.5-20 Hz)
    and corrected for baseline over a 250-ms window during fixation at the beginning of the trial.
-"""
 
+
+   """
+from __future__ import annotations
+from typing import TYPE_CHECKING
 from os.path import join, expanduser
 import glob, os
 from mne.io import read_raw_bdf
 from mne.channels import make_standard_montage
 import mne
 from experiment.triggers import Triggers
+if TYPE_CHECKING:
+    from mne.io.edf.edf import RawEDF
 
 data_dir = expanduser('~/data/EMLsergent2005/')
 sub = 'sub-UOBC001'
 eeg_dir = join(data_dir, sub)
 deriv_dir = join(data_dir, 'derivatives', 'mne', sub)
-raw_fpaths = glob.glob(join(eeg_dir, '*.bdf'))
-assert len(raw_fpaths) == 1
+raw_fpath = join(eeg_dir, f'{sub}_eeg.bdf')
+
 
 os.makedirs(deriv_dir, exist_ok=True)
 
 
 ## load raw data 
-raw = read_raw_bdf(raw_fpaths[0])
+raw = read_raw_bdf(raw_fpath)
+#raise ValueError
 
 ## get rid of empty channels and mark channel types
-unused_channels = ['GSR1', 'GSR2', 'Erg1', 'Erg2', 'Resp', 'Plet', 'Temp',
-    'EXG7', 'EXG8']
-raw = raw.drop_channels(unused_channels)
+raw: RawEDF = raw.drop_channels(['EXG7', 'EXG8']) # type: ignore
 eog_channels = ['EXG3', 'EXG4', 'EXG5', 'EXG6']
 raw.set_channel_types(mapping=dict([(c, 'eog') for c in eog_channels]))
-
-## Bad channels and segments
-raw.info['bads'] = ['A25', 'A26'] 
 
 ## pick channels to be filtered
 filter_picks = mne.pick_types(raw.info, eeg=True, eog=True, stim=False)
@@ -62,9 +72,8 @@ mastoid_channels = ['EXG1', 'EXG2'] # best for biosemi, but methods plans to use
 raw.set_eeg_reference(ref_channels=mastoid_channels)
 
 ## find triggers
-events = mne.find_events(raw)
-# events = mne.find_events(raw, mask=65280, mask_type='not_and')
-events[:, 2] = events[:, 2]-65280
+events = mne.find_events(raw, mask=2**17 -256, mask_type='not_and', consecutive=True)
+
 
 # trigger definition (only epoch the T1 and T2 triggers)
 event_ids = dict([(k, v) for (k, v) in Triggers().asdict().items() if v > 12])
