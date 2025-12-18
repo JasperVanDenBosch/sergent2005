5351ed13ef08e7ed221110502cce7fda7cfcd3be
Jasper van den Bosch <japsai@gmail.com>
2025-12-18 18:39:45 +0000

    creating events file

--- analysis/bidsify.py
+++ analysis/bidsify.py
@@ -7,6 +7,8 @@ Looks for data in "sourcedata" folder
 - mark bad segments
 - bad channels
 """
+from __future__ import annotations
+from typing import Tuple, Any, Dict
 from os.path import expanduser, join
 from os import makedirs
 from glob import glob
@@ -15,20 +17,19 @@ from mne.io import read_raw_bdf
 from mne import find_events
 from numpy import diff
 from string import Template
-from pandas import read_csv, DataFrame
+from pandas import read_csv, DataFrame, Series
+from event import event_dict, EventType
+
+SFREQ = 2048
+
+
 
 BUFFER_SEC = 5
 TASK_DESC = {
     'blink': 'Attentional Blink paradigm',
 }
-COLS = {
-    'target': 'trial_type',
-    'TriggerNum': 'value',
-    'TrialKeyResp.rt': 'rt',
-    'imgfileP2': 'identity',
-    'Trial.started': 'onset'
-}
-TASK = 'blink'
+TASK = 'ab'
+
 
 CHANNELS = [
     dict(name='EXG1', type='REF', units='uV', description='left mastoid'),
@@ -58,8 +59,8 @@ for source_dir in source_dirs:
     sub_dir = join(data_dir, sub)
     eeg_dir = join(sub_dir, 'eeg')
     makedirs(eeg_dir, exist_ok=True)
-    beh_dir = join(sub_dir, 'beh')
-    makedirs(beh_dir, exist_ok=True)
+    # beh_dir = join(sub_dir, 'beh')
+    # makedirs(beh_dir, exist_ok=True)
 
 
     bdf_fpath = glob(join(source_dir, '*.bdf'))[0]
@@ -80,10 +81,51 @@ for source_dir in source_dirs:
     DataFrame(channels).to_csv(fpath_chan, sep='\t', index=False)
 
 
+    ## process event file
+    csv_files = glob(join(source_dir, f'*_trials.csv'))
+    assert len(csv_files) == 1
+    trials_df = read_csv(csv_files[0])
+
     evt = find_events(raw, shortest_event=0)
 
     mask = sum([2**i for i in (8,9,10,11,12,13,14,15,16)])
     evt = find_events(raw, mask=mask, mask_type='not_and')
+
+
+
+    """
+
+    At least in UOLM001, the trigger for the task (1,2,11,12) 
+    seems to often be missing.
+
+    """
+    e = 0
+    events = []
+    for t, trial in trials_df.iterrows():
+
+        te = 0
+        assert trial.t1_trigger == evt[e+te, 2]
+        events.append(event_dict(EventType.T1, trial, tuple(evt[e+te, :]), SFREQ))
+
+
+        te += 1
+        assert trial.t2_trigger == evt[e+te, 2]
+        events.append(event_dict(EventType.T2, trial, tuple(evt[e+te, :]), SFREQ))
+
+        te += 1
+        for _ in range(2):
+            if e+te < evt.shape[0]:
+                print(evt[e+te, 2])
+                if evt[e+te, 2] in (1, 2, 11, 12):
+                    events.append(event_dict(EventType.TASK, trial, tuple(evt[e+te, :]), SFREQ))
+                    te += 1
+        if te <= 2:
+            #print(f'No prompt trigger in trial {t}')
+            pass
+        
+        e += te
+
+    DataFrame(events).to_csv(join(eeg_dir, f'{sub}_task-{TASK}_events.tsv'), sep='\t', index=False)
 
 
     raise ValueError
---
+++ analysis/event.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+from typing import Tuple, Any, Dict
+from enum import Enum
+from pandas import Series
+
+class EventType(Enum):
+     TASK = 0
+     T1 = 1
+     T2 = 2
+
+
+def event_dict(kind: EventType, trial: Series, evt: Tuple[int, int, int], sfreq: int) -> Dict[str, Any]:
+    """Make a BIDS events row from the trial row and the MNE event
+    """
+    onset = round(evt[0] / sfreq, 3)
+    if kind == EventType.TASK:
+        return dict(
+            onset = evt[0] / sfreq,
+            sample = evt[0],
+            value = evt[2],
+            trial_type='prompt_t1' if evt[2] in (1, 11) else 'prompt_t2',
+            trial_index=trial['Unnamed: 0'],
+            phase=trial.phase,
+            dual_task=trial.task=='dual',
+        )
+    else:
+        if trial.task=='dual':
+            id_string = {0.0: 'XOOX', 1.0: 'OXXO'}[trial.id_choice]
+            correct = id_string == trial.target1
+        else:
+            correct = None
+        return dict(
+            onset = evt[0] / sfreq,
+            sample = evt[0],
+            value = evt[2],
+            trial_type=kind.name.lower(),
+            trial_index=trial['Unnamed: 0'],
+            phase=trial.phase,
+            dual_task=trial.task=='dual',
+            t2presence=trial.t2presence,
+            soa_long=trial.soa_long,
+            stimulus=trial.target1 if kind==EventType.T1 else trial.target2,
+            vis_rating=trial.vis_rating,
+            vis_init=trial.vis_init,
+            correct=correct,
+        )
